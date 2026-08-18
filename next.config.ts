3b273be7838ee7f16dcbddbd3d80e5f2815ed706
shinas101 <shinassamooncvd@gmail.com>
2026-08-18 13:19:29 +0530

    fix(config): configure outputFileTracingRoot to eliminate build warnings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["127.0.0.1"],
+  outputFileTracingRoot: path.join(__dirname),
+  allowedDevOrigins: ["127.0.0.1", "localhost"],
 };
 
 export default nextConfig;
